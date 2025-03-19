36410de21ed0784db4fe6e65be6a1f31f566b2a7
bunohey <s4065329@student.rmit.edu.au>
2025-03-20 09:49:25 +1100

    a

--- Practice/week3/colourPickerFeature/script.js
+++ Practice/week3/colourPickerFeature/script.js
@@ -28,11 +28,40 @@ function setSquareColour(newColour){
 /* below is an example call to the function : because its just in the script it'll run when the page is loaded */
 setSquareColour('red');
 
-let colourPicker = document.getElementById("colour-picker");
+const colourPicker()
 
-colourPicker.addEventListener("input", PickingColour);
-
-function PickingColour(e) {
-    //console.log(e.target);
-    setSquareColour(e.target.value);
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
 }
+
+function randomRgbColor() {
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return [r,g,b];
+}
+
+function randomHexColor() {
+    let [r,g,b] =randomRgbColor();
+
+    let hr = r.toString(16).padStart(2, '0');
+    let hg = g.toString(16).padStart(2, '0');
+    let hb = b.toString(16).padStart(2, '0');
+
+    return "#" + hr + hg + hb;
+}
+
+function changeColor() {
+  let hex = randomHexColor();
+  document.getElementById('color').value = hex;
+  document.getElementById('text').innerHTML = hex;
+}
+
+function clickHandler(event) {
+  changeColor();
+  event.preventDefault();
+}
+
+document.addEventListener('click', clickHandler);
+
+changeColor();
